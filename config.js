7aee2b7bcb67c3aef0e55cf10075b19c4f9f8653
IkmalMtp2 <105196002+IkmalMtp2@users.noreply.github.com>
2022-07-04 09:09:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -33,13 +33,13 @@ global.nomer = 'https://wa.me/6282127081926' //ganti jadi nomor mu
 global.price1 = '𝐍𝐈𝐇 𝐁𝐀𝐍𝐆 𝐋𝐈𝐒𝐓 𝐒𝐄𝐖𝐀𝐍𝐘𝐀\n\n》1 minggu Rp 2.000\n 1GRUP\n》1 Bulan Rp 6.000\n 1 GRUP\n》PERMANEN Rp 15.000\n1 GRUP'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti pakai foto bot mu
-global.fotonya3 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti
-global.fotonya4 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti
-global.fotonya5 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti
-global.fotonya6 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini ganti pakai foto sosialmedia mu
-global.fotonya7 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti
+global.fotonya1 = 'https://telegra.ph/file/e6ecaa9d05e08e618d4f1.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/e6ecaa9d05e08e618d4f1.jpg' //ini juga ganti pakai foto bot mu
+global.fotonya3 = 'https://telegra.ph/file/e6ecaa9d05e08e618d4f1.jpg' //ini juga ganti
+global.fotonya4 = 'https://telegra.ph/file/e6ecaa9d05e08e618d4f1.jpg' //ini juga ganti
+global.fotonya5 = 'https://telegra.ph/file/e6ecaa9d05e08e618d4f1.jpg' //ini juga ganti
+global.fotonya6 = 'https://telegra.ph/file/e6ecaa9d05e08e618d4f1.jpg' //ini ganti pakai foto sosialmedia mu
+global.fotonya7 = 'https://telegra.ph/file/e6ecaa9d05e08e618d4f1.jpg' //ini juga ganti
 global.fotonya8 = 'https://telegra.ph/file/14ec0e0c93af4812d1fd7.jpg' //foto session
 global.donasi = 'https://telegra.ph/file/c171460c1e60d22ea379e.jpg' //ganti pakai poto payment atau apa
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
